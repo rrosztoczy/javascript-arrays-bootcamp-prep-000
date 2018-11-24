e5c30b043c731b88a260367a59ef91fcff80b652
Ryan Rosztoczy <rrosztoczy@gmail.com>
2018-11-24 16:40:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -62,3 +62,15 @@ return array
 function accessElementInArray(array,index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var smallerArray = array.slice(1)
+  return smallerArray
+}
+
+dest
